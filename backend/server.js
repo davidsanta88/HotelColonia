d6e087efb65a9de933fed2efdf2c8323468781e5
dsanta <david.santa@selenesoftware.co>
2026-03-25 19:41:20 -0500

    FIX: ReferenceError path is not defined in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const path = require('path');
 const connectDB = require('./config/db');
 
 // Iniciar app
